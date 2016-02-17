5beb91503dc7dcfbcf07c59a1cc74583ff0a4681
tlazarus88 <trevor.das.lazarus@gmail.com>
2016-02-16 23:48:44 -0500

    Second Round
    Locations page sidebar in progress

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//$(document).ready(function(){
+	//var  bkgtitle = $('h2').innerHTML;
+	//var  bkgimg = ("../images/"bkgtitle".jpg");
+	//console.log('bkgtitle');
+	//console.log('bkgimg');
+var bgname;
+
+$('h2').html()=bgname;
+console.log(bgname);
+
+//$('.destination-splash').css('background-image', 'url(../images/'bgname'.jpg)');
