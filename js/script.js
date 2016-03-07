ec9eaef92c39425361bd7b822d0591eab3d35b9a
tlazarus88 <trevor.das.lazarus@gmail.com>
2016-03-06 21:57:02 -0500

    quiz animation complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+var quizQuestions = ['.question-1','.question-2','.question-3','.question-4','.question-5','.question-6'];
+
+//var quizResponses = ['.responses-1','.responses-2','.responses-3','.responses-4','.responses-5','.responses-6'];
+
+var i = 0;
+
+$('.quiz-response').on('click', function () {
+	$(quizQuestions[i]).fadeOut(1000, "linear", function () {
+		i++;
+		if (i<5) {
+			$(quizQuestions[i]).fadeIn(1000, "linear");
+		} else if (i=5) {
+			$(window).load("http://google.com");
+		};
+	});
+});
+//setup array of variables
+
+//capture click of button
+
+//hide current question
+
+//fade in new question
+
+
+
 
 // creates generic map
 /*function initMap() {
@@ -13,6 +39,7 @@
  *   and displaying the contents in a side panel instead of
  *   an InfoWindow
  */
+ /*
 
 var map;
 var src = 'https://developers.google.com/maps/tutorials/kml/westcampus.kml';
@@ -20,6 +47,7 @@ var src = 'https://developers.google.com/maps/tutorials/kml/westcampus.kml';
 /**
  * Initializes the map and calls the function that creates polylines.
  */
+ /*
 function initialize() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(-19.257753, 146.823688),
@@ -34,6 +62,7 @@ function initialize() {
  * results in the balloon content being loaded into the right-hand div.
  * @param {string} src A URL for a KML file.
  */
+ /*
 function loadKmlLayer(src, map) {
   var kmlLayer = new google.maps.KmlLayer(src, {
     suppressInfoWindows: true,
@@ -45,4 +74,4 @@ function loadKmlLayer(src, map) {
     var testimonial = document.getElementById('capture');
     testimonial.innerHTML = content;
   });
-}
+}*/
