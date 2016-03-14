20fe74634b466e559447c0ae3066f2116f5034d2
tlazarus88 <trevor.das.lazarus@gmail.com>
2016-03-14 11:23:13 -0400

    bar-tile labels added, map completed
    all of the bar-tile listeners added, maps completed in wheated to be
    used across sites

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-/*function userSubmit() {
-	$("form").on('submit', function() {
-		str = $('#city-type').val();
-		city = $.trim(str);
-*/
-var quizQuestions = ['.question-1','.question-2','.question-3','.question-4','.question-5','.question-6'];
 
-//var quizResponses = ['.responses-1','.responses-2','.responses-3','.responses-4','.responses-5','.responses-6'];
+var quizQuestions = ['.question-1','.question-2','.question-3','.question-4','.question-5','.question-6'];
 
 var i = 0;
 
+//setup array of variables
 var food;
 var happyHour;
 var vibe;
@@ -16,66 +11,107 @@ var groupSize;
 var outdoors;
 var creditCard;
 
+//capture click of button
 $('.response').on('click', function (e) {
   e.preventDefault();
-		$(quizQuestions[i]).fadeOut(1000, "linear", function () {
+		//hide current question
+    $(quizQuestions[i]).fadeOut(500, "linear", function () {
 		i++;
+    //fade in new question
 		if (i<=5) {
-			$(quizQuestions[i]).fadeIn(1000, "linear");
+			$(quizQuestions[i]).fadeIn(500, "linear");
 		} else {
-			//alert(food);
-      //$(window).load("index.html");
-      //$(window).load("bars.html/?asp.'+food+'&'+happyHour+'&'+vibe+'&'+groupSize+'&'+outdoors+'&'+creditCard+'");
+      window.location.href = "bars.html";
 		};
 	});
 });
 
+//capture and save quiz values
 $(".question-1").on("click", ":submit", function() {
     food = $(this).val();
+    localStorage.setItem('food', food);
+    //localStorage.getItem('food');
     //alert(food);
     console.log(food);
 });
 
 $(".question-2").on("click", ":submit", function() {
     happyHour = $(this).val();
+    localStorage.setItem('happyHour', happyHour);
     //alert(happyHour);
     console.log(happyHour);
 });
 
 $(".question-3").on("click", ":submit", function() {
     vibe = $(this).val();
+    localStorage.setItem('vibe', vibe);
     //alert(vibe);
     console.log(vibe);
 });
 
 $(".question-4").on("click", ":submit", function() {
     groupSize = $(this).val();
+    localStorage.setItem('groupSize', groupSize);
     //alert(groupSize);
     console.log(groupSize);
 });
 
 $(".question-5").on("click", ":submit", function() {
     outdoors = $(this).val();
+    localStorage.setItem('outdoors', outdoors);
     //alert(outdoors);
     console.log(outdoors);
 });
 
 $(".question-6").on("click", ":submit", function() {
     creditCard = $(this).val();
+    localStorage.setItem('creditCard', creditCard);
     console.log(food,happyHour,vibe,groupSize,outdoors,creditCard);
-    var arr = [ food, happyHour, vibe, groupSize, outdoors, creditCard];
+    //var arr = [ food, happyHour, vibe, groupSize, outdoors, creditCard];
     //$.makeArray( food, happyHour, vibe, groupSize, outdoors, creditCard )
-    alert(arr);
+    //alert(arr);
 });
-//setup array of variables
 
-//capture click of button
+//filter rules for "Bars" page
+$('input').on('change', function() {
+    if ($('#yesFood').is(':checked')) {
+        $('.noOutdoors').animate({width:'toggle'},500);
+        $('.yesOutdoors').fadeIn(500, "linear");
+    } else if ($('#noFood').is(':checked')) {
+        $('.yesOutdoors').animate({width:'toggle'},500);
+        $('.noOutdoors').fadeIn(500, "linear");
+    } 
+})
+$('input').on('change', function() {
+    if ($('#yesHh').is(':checked')) {
+        $('.noHh').animate({width:'toggle'},500);
+        $('.yesHh').fadeIn(500, "linear");
+    } else if ($('#noHh').is(':checked')) {
+        $('.yesHh').animate({width:'toggle'},500);
+        $('.noHh').fadeIn(500, "linear");
+    } 
+})
 
-//hide current question
+//reset bar query parameters
+$('#bars-nav').on('click', function(e){
+  e.preventDefault();
+  food = 'naFood';
+  happyHour = 'naHh';
+  vibe = 'naVibe';
+  groupSize = 'naGroup';
+  outdoors = 'naOutdoors';
+  creditCard = 'naCredit';
+  localStorage.setItem('food', food);
+  localStorage.setItem('happyHour', happyHour);
+  localStorage.setItem('vibe', vibe);
+  localStorage.setItem('groupSize', groupSize);
+  localStorage.setItem('outdoors', outdoors);
+  localStorage.setItem('creditCard', creditCard);
 
-//fade in new question
+ // console.log(food,happyHour,vibe,groupSize,outdoors,creditCard);
 
 
+});
 
 
 // creates generic map
